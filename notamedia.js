3fb9e28e4048fa0a31c2083e39a431e81dacc394
NullBeta001 <fabiooli221297@gmail.com>
2022-01-07 06:54:42 -0300

    Teste de média de notas

---
+++ notamedia.js
@@ -0,0 +1,10 @@
+let n1 = parseFloat(console.log("Primeira nota:"));
+let n2 = parseFloat (console.log("Segunda nota:"));
+let n3 = parseFloat(console.log("Terceira nota:"));
+
+let media = (n1+n2+n3)/3;
+if(media>=7){
+    console.log("Aprovado!");
+}else{
+    console.log("Reprovado!");
+}
